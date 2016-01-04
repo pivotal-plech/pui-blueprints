835e7aeaa7e57d60d42b332dd4f2ffc583e3f2db
Spencer Hurst <pair+spencer@pivotallabs.com>
2016-01-04 13:17:44 -0500

    update home to include scaffold of blueprints

--- app/scripts/components/hero.jsx
+++ app/scripts/components/hero.jsx
@@ -6,7 +6,9 @@ var Hero = React.createClass({
 
     return (
       <div className="hero">
-        <p className="type-dark-11 txt-c h1 mvn em-low" style={{maxWidth: "800px", margin: "0 auto"}}>Value prop with propy like things.</p>
+        <p className="type-dark-11 txt-c h1 mvn em-low" style={{maxWidth: "800px", margin: "0 auto"}}>
+          Pivotal UI Blueprints are a collection concepts and components.
+        </p>
       </div>
     );
   }
--- app/scripts/components/home.jsx
+++ app/scripts/components/home.jsx
@@ -9,53 +9,56 @@ var TileLayout = require('pui-react-tile-layout');
 var BasicPanelAlt = require('pui-react-panels').BasicPanelAlt;
 
 var Header = require('./header');
-
-// Firebase
-var Firebase = require("firebase");
-var ReactFireMixin = require("reactfire");
-
+var mediaAlignmentImage = <img href='http://www.google.com' src='http://placehold.it/100x100'  alt='A sample media object' />;
 
 var Home = React.createClass({
 
-  mixins: [ReactFireMixin, Navigation],
+  mixins: [Navigation],
 
-  getInitialState: function() {
-    return {
-      products: []
-    };
-  },
-
-  componentWillMount: function() {
-    var myFirebaseRef = new Firebase("https://pivotal-docs.firebaseio.com/products");
-    this.bindAsArray(myFirebaseRef, "products");
-  },
 
   render: function() {
-
-    var products = this.state.products.map(function(product) {
-
-      return (
-        <TileLayout.Item key={ product['.key'] }>
-          <a href="/#/product" className="panel-link">
-            <BasicPanelAlt className="pvxl">
-              <p className="em-high mvn h3 type-dark-1 txt-c">Pivotal</p>
-              <h3 className="h3 em-low type-dark-1 txt-c">{ product.productName }</h3>
-            </BasicPanelAlt>
-          </a>
-        </TileLayout.Item>
-      );
-
-    });
-
     return (
       <div className="wrap">
         <div className="home">
           <Hero />
           <div className="container">
-            <h1 className="em-low">Pivotal Software</h1>
+            <h1 className="em-low">Blueprints</h1>
             <hr />
-            <TileLayout columns={3}>
-              {products}
+            <TileLayout columns={2}>
+              <TileLayout.Item>
+                <a href="#/pageheader">
+                  <BasicPanelAlt className="mvn">
+                    <h3 className="type-dark-1 mvn">Page Header</h3>
+                    <Flag leftImage={mediaAlignmentImage} leftMediaSpacing='xlarge'>
+                      A responsive product grid layout with touch-friendly Flickity galleries and Isotope-powered filter functionality.	...
+                    </Flag>
+                  </BasicPanelAlt>
+                </a>
+              </TileLayout.Item>
+              <TileLayout.Item>
+                <BasicPanelAlt className="mvn">
+                  <h3 className="type-dark-1 mvn">Page Stack Navigation</h3>
+                  <Flag leftImage={mediaAlignmentImage} leftMediaSpacing='xlarge'>
+                    A responsive product grid layout with touch-friendly Flickity galleries and Isotope-powered filter functionality.	...
+                  </Flag>
+                </BasicPanelAlt>
+              </TileLayout.Item>
+              <TileLayout.Item>
+                <BasicPanelAlt className="mvn">
+                  <h3 className="type-dark-1 mvn">Page Stack Navigation</h3>
+                  <Flag leftImage={mediaAlignmentImage} leftMediaSpacing='xlarge'>
+                    A responsive product grid layout with touch-friendly Flickity galleries and Isotope-powered filter functionality.	...
+                  </Flag>
+                </BasicPanelAlt>
+              </TileLayout.Item>
+              <TileLayout.Item>
+                <BasicPanelAlt className="mvn">
+                  <h3 className="type-dark-1 mvn">Page Stack Navigation</h3>
+                  <Flag leftImage={mediaAlignmentImage} leftMediaSpacing='xlarge'>
+                    A responsive product grid layout with touch-friendly Flickity galleries and Isotope-powered filter functionality.	...
+                  </Flag>
+                </BasicPanelAlt>
+              </TileLayout.Item>
             </TileLayout>
           </div>
         </div>
