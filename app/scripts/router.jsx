4a314116135bb7dce6b8911453468124676505cf
Spencer Hurst <pair+spencer@pivotallabs.com>
2016-03-14 10:56:02 -0400

    update pui

--- app/scripts/router.jsx
+++ app/scripts/router.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 var Router = require('react-router');
 var Route = Router.Route;
 var DefaultRoute = Router.DefaultRoute;
@@ -6,17 +7,19 @@ var DefaultRoute = Router.DefaultRoute;
 var Layout = require('./components/layout');
 var Home = require('./components/home');
 var PageHeader = require('./components/blueprints/pageHeader');
+var Panels = require('./components/blueprints/panels');
 
 var routes = (
   <Route name="layout" path="/" handler={Layout}>
     <DefaultRoute handler={Home} />
     <Route handler={PageHeader} name="/pageheader" />
+    <Route handler={Panels} name="/panels" />
   </Route>
 );
 
 exports.start = function() {
 
   Router.run(routes, function (Handler) {
-    React.render(<Handler />, document.getElementById('content'));
+    ReactDOM.render(<Handler />, document.getElementById('content'));
   });
 }
